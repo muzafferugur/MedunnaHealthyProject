a85d47eab5969d0b2e0c6a2f27ec4761fce6a7e6
Hus.Nht <hus.nht@gmail.com>
2022-11-24 11:36:55 +0300

    24.11.2022

--- src/test/java/pages/MedunnaPage.java
+++ src/test/java/pages/MedunnaPage.java
@@ -4,49 +4,49 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
-public class MedunnaPage {
-    public MedunnaPage() {
-        PageFactory.initElements(Driver.getDriver(), this);
-    }
+public class MedunnaPage{
+    public MedunnaPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+        }
 //US-001 (11-110)
-    @FindBy(id = "account-menu") public WebElement accountMenu;
-    @FindBy(xpath = "//*[text()='Register']") public WebElement register;
-    @FindBy(xpath = "//input[@name='ssn']") public WebElement ssnBox;
-    @FindBy(xpath = "(//*[@class='invalid-feedback'])[1]") public WebElement SSNFeedback;
-    @FindBy(xpath = "//*[.='Your SSN is required.']") public WebElement SSNRequiredFeedback;
-    @FindBy(xpath = "//*[.='Your FirstName is required.']") public WebElement firstNameRequiredFeedback;
-    @FindBy(xpath = "//*[.='Your LastName is required.']") public WebElement lastNameRequiredFeedback;
-    @FindBy(xpath = "(//input[@class='is-touched is-dirty av-valid form-control'])[1]") public WebElement SSNValidFeedback;
-    @FindBy(xpath = "//input[@name='firstName']") public WebElement firstNameBox;
-    @FindBy(xpath = "(//div[@class='invalid-feedback'])[1]")  public WebElement firstNameFeedback;
-    @FindBy(xpath = "//input[@name='lastName']") public WebElement lastNameBox;
-    @FindBy(xpath = "(//div[@class='invalid-feedback'])[2]") public WebElement lastNameFeedback;
-    @FindBy(xpath = "//input[@name='username']") public WebElement userNameBox;
-    @FindBy(xpath = "//*[.='Your username is required.']") public WebElement userNameRequiredFeedback;
-    @FindBy(xpath = "//input[@name='email']") public WebElement emailBox;
-    @FindBy(xpath = "//*[.='Your email is required.']") public WebElement emailRequiredFeedback;
-    @FindBy(xpath = "//*[@id=\"register-form\"]/div[5]/div") public WebElement emailInvalidFeedback;
-    @FindBy(xpath = "//*[@id='firstPassword']") public WebElement newPasswordBox;
-    @FindBy(xpath = "//*[.='Your password is required to be at least 4 characters.']") public WebElement invalidPasswordFeedback;
-    @FindBy(xpath = "//li[@style='background-color: rgb(255, 0, 0);']") public WebElement newPasswordRegisterPageFirstColor;
-    //her basamak ayri ayri locate edilmemis 1. basamak ve diigerleri olarak locate edilmis, bu bir bug olmasa da product owner a bunun requirement yetreince iyi "clear"yansittigini söylemek fena olmaz.
-    @FindBy(xpath = "//li[@style='background-color: rgb(221, 221, 221);']") public WebElement getNewPassWordRegisterPageStrengthColor;
-    @FindBy(xpath = "//input[@placeholder='Confirm the new password']")  public WebElement confirmationPasswordRegisterPage;
-    @FindBy(xpath = "//div[text()='The password and its confirmation do not match!']") public WebElement invalidPasswordConfirmationMessageRegisterPage;
-    @FindBy(id = "register-submit") public WebElement buttonRegisterPage;
-    @FindBy(xpath = "//*[text()='Registration saved!']") public WebElement successMessageToastContainerRegisterPage;
-
-
-
-
-
-
-
-
-
-
-
-
+   @FindBy(id = "account-menu")   public WebElement accountMenu;
+    @FindBy(xpath = "//*[text()='Register']")   public WebElement register;
+    @FindBy(xpath = "//input[@name='ssn']")   public WebElement ssnBox;
+    @FindBy(xpath = "(//*[@class='invalid-feedback'])[1]")   public WebElement SSNFeedback;
+    @FindBy(xpath = "//*[.='Your SSN is required.']")   public WebElement SSNRequiredFeedback;
+    @FindBy(xpath = "//*[.='Your FirstName is required.']")   public WebElement firstNameRequiredFeedback;
+    @FindBy(xpath = "//*[.='Your LastName is required.']")   public WebElement lastNameRequiredFeedback;
+    @FindBy(xpath = "(//input[@class='is-touched is-dirty av-valid form-control'])[1]")   public WebElement SSNValidFeedback;
+    @FindBy(xpath = "//input[@name='firstName']")    public WebElement firstNameBox;
+    @FindBy(xpath = "(//div[@class='invalid-feedback'])[1]")   public WebElement firstNameFeedback;
+    @FindBy(xpath = "//input[@name='lastName']")      public WebElement lastNameBox;
+    @FindBy(xpath = "(//div[@class='invalid-feedback'])[2]")     public WebElement lastNameFeedback;
+    @FindBy(xpath = "//input[@name='username']")    public WebElement userNameBox;
+    @FindBy(xpath = "//*[.='Your username is required.']")   public WebElement userNameRequiredFeedback;
+    @FindBy(xpath = "//input[@name='email']")    public WebElement emailBox;
+    @FindBy(xpath = "//*[.='Your email is required.']")   public WebElement emailRequiredFeedback;
+    @FindBy(xpath = "//*[@id=\"register-form\"]/div[5]/div")   public WebElement emailInvalidFeedback;
+    @FindBy(xpath = "//*[@id='firstPassword']")   public WebElement newPasswordBox;
+    @FindBy(xpath = "//*[.='Your password is required to be at least 4 characters.']")
+    public WebElement invalidPasswordFeedback;
+    @FindBy(xpath = "//li[@style='background-color: rgb(255, 0, 0);']")
+    public WebElement newPasswordFirstColor;
+    @FindBy(xpath = "//li[@style='background-color: rgb(255, 153, 0);']")
+    public WebElement newPasswordSecondColor;
+    @FindBy(xpath = "//li[@style='background-color: rgb(153, 255, 0);']")
+    public WebElement newPasswordFourthColor;
+    public WebElement newPasswordFifthColor;
+    @FindBy(xpath = "//input[@name='firstPassword']") public WebElement newPasswordSec;
+    @FindBy(xpath = "//input[@name='username']") public WebElement username;
+    @FindBy(xpath = "//*[text()='Sign in']") public WebElement signIn;
+    @FindBy(xpath = "//*[text()='Administration']") public WebElement girisText;
+    @FindBy(xpath = "//*[@id='rememberMe']") public WebElement rememberMe;
+    @FindBy(xpath = "//*[text()='Did you forget your password?']") public WebElement didYouForget;
+    @FindBy(xpath = "//*[text()='Enter the email address you used to register']") public WebElement resetPassword;
+    @FindBy(xpath = "//*[text()='Register a new account']") public WebElement accountGo;
+    @FindBy(xpath = "//*[text()='Cancel']") public WebElement cancel;
+    @FindBy(xpath = "//*[@id='app-view-container']") public WebElement anaSayfa;
+    @FindBy(xpath = "Sign out") public WebElement signOut;
 
 
 
@@ -1607,7 +1607,6 @@ public class MedunnaPage {
 
 
 
-
 //US-017 (1611-1710)
 
 
@@ -2709,6 +2708,7 @@ public class MedunnaPage {
 
 
 //US-028 (2711-2810)
+
 
 
 
