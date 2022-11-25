0c49f63d967d3967c535d69ccf33ff0b219b18cf
Kübra Basaran <kubraba35@gmail.com>
2022-11-25 15:42:44 +0300

    kubra

--- src/test/java/pages/MedunnaPage.java
+++ src/test/java/pages/MedunnaPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.sql.Driver;
+
 public class MedunnaPage{
     public MedunnaPage(){
         PageFactory.initElements(Driver.getDriver(),this);
